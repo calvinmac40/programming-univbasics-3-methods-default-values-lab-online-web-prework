843fb484a02cfeea995dc5ed188ae7f7fd8b490c
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-12 12:04:11 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,15 @@
-def meal_choice(veg1, veg2, protein = "meat")
-   veg1 = greens
-   veg2 = carrots
-   puts "What a nutritious meal!"
-   puts "A plate of #{protein} with #{veg1} and #{veg2}."
- end
- 
- 
+# def meal_choice(veg1, veg2, protein = 'meat')
+#   puts "What a nutritious meal!"
+#   puts "A plate of #{protein} with #{veg1} and #{veg2}."
+#   puts "What a delicious meal"
+#   return true
+# end
+
+# meal_choice("broccoli","macaroni")
+
+def meal_choice(veg1, veg2, protein='meat')
+  puts "What a nutritious meal!"
+  meal = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal
+  return meal # return here is optional. Ruby methods automatically return the last expression
+end
